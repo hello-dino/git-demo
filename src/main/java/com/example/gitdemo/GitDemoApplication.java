bde28aad251a3077a83d8762a2f5fed8e872eeb5
hello-dino <waahhh@naver.com>
2021-09-30 06:01:55 +0900

    signin here

--- src/main/java/com/example/gitdemo/GitDemoApplication.java
+++ src/main/java/com/example/gitdemo/GitDemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GitDemoApplication {
 
     public static void main(String[] args) {
-        //singin
+        //singin here
         SpringApplication.run(GitDemoApplication.class, args);
     }
 
